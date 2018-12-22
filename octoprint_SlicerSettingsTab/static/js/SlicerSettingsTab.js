ae60d1a96a63fb34f249a0dfcebda27a1b1c39d4
tjjfvi <tjjfvi@tjjfvi.com>
2018-12-22 20:15:58 +0000

    added copy button to key & value tds

--- octoprint_SlicerSettingsTab/static/js/SlicerSettingsTab.js
+++ octoprint_SlicerSettingsTab/static/js/SlicerSettingsTab.js
@@ -1,51 +1,11 @@
 $(function() {
-	function Setting(key, value){
-		var self = this;
+	OCTOPRINT_VIEWMODELS.push({
+				construct: SlicerSettingsTabViewModel,
+				elements: ["#tab_plugin_SlicerSettingsTab"]
+		});
 
-		self.key = ko.observable(key.split("\\n").join("\n"));
-		self.value = ko.observable(value.split("\\n").join("\n"));
-
-		self.escape = text => ko.pureComputed(() =>
-			$("<span>").text(text()).html()
-		);
-
-		console.log([value, self.value()]);
-
-		self.filterHelpers = filterString => {
-			let splitFilterString = ko.pureComputed(() =>
-				filterString().split(/\s+/).filter(s => s)
-			);
-
-			let self = Object.assign(Object.create(this), {
-				match: ko.pureComputed(() =>
-					splitFilterString().every(s =>
-						key.includes(s) ||
-						value.includes(s)
-					)
-				),
-				highlight: text => ko.pureComputed(() =>
-					splitFilterString().reduce((text, s) =>
-						text.replace(s, `<b>${s}</b>`)
-					, text())
-				),
-				order: ko.pureComputed(() =>
-					splitFilterString().map(s => [
-						  key.includes(s) ? s.length /   key.length : 0,
-						value.includes(s) ? s.length / value.length : 0,
-					]).reduce((a, b) => a.map((n, i) => n + b[i]), [0, 0])
-				),
-				orderInt: ko.pureComputed(() =>
-					-Math.round(1e5 * self.order().reduce((a, b) => a * 10000 + b, 0))
-				),
-			});
-
-			return self;
-		}
-
-	}
-
-    function SlicerSettingsTabViewModel(parameters) {
-        var self = this;
+	function SlicerSettingsTabViewModel(parameters) {
+				var self = this;
 
 		console.log(this);
 
@@ -53,22 +13,6 @@ $(function() {
 
 		self.settings = ko.observableArray([]);
 		self.sortedSettings = ko.observableArray([]);
-
-		// self.settings.subscribe(v =>
-		// 	self.sortedSettings(self.settings().map(s => s.filterHelpers(self.filterString)))
-		// );
-		//
-		// self.filterString.subscribe(() => setTimeout(() => console.log("hi") || self.sortedSettings.sort((a, b) =>
-		// 	!a.match() ?
-		// 	 	1 :
-		// 	!b.match() ?
-		// 		-1 :
-		// 		[b, a]
-		// 			.map(c => c.order())
-		// 			.reduce((b, a) => b.map((n, i) => n - a[i]))
-		// 			.reduce((a, b) => a || b, 0)
-		// 		|| (b.key() < a.key() ? 1 : -1)
-		// ), 0));
 
 		self.updating = ko.observable(false);
 		self.fileSelected = ko.observable(true);
@@ -113,10 +57,101 @@ $(function() {
 
 		self.onEventFileSelected = self.refresh;
 		self.onEventFileDeselected = () => self.fileSelected(false);
-    }
+		}
 
-    OCTOPRINT_VIEWMODELS.push({
-        construct: SlicerSettingsTabViewModel,
-        elements: ["#tab_plugin_SlicerSettingsTab"]
-    });
+	function Setting(key, value){
+		var self = this;
+
+		self.key = ko.observable(key.split("\\n").join("\n"));
+		self.value = ko.observable(value.split("\\n").join("\n"));
+
+		self.escape = text => ko.pureComputed(() =>
+			$("<span>").text(text()).html()
+		);
+
+		self.copyButton = text => new CopyButton(text);
+
+		console.log([value, self.value()]);
+
+		self.filterHelpers = filterString => {
+			let splitFilterString = ko.pureComputed(() =>
+				filterString().split(/\s+/).filter(s => s)
+			);
+
+			let self = Object.assign(Object.create(this), {
+				match: ko.pureComputed(() =>
+					splitFilterString().every(s =>
+						key.includes(s) ||
+						value.includes(s)
+					)
+				),
+				highlight: text => ko.pureComputed(() =>
+					splitFilterString().reduce((text, s) =>
+						text.replace(s, `<b>${s}</b>`)
+					, text())
+				),
+				order: ko.pureComputed(() =>
+					splitFilterString().map(s => [
+							key.includes(s) ? s.length /	 key.length : 0,
+						value.includes(s) ? s.length / value.length : 0,
+					]).reduce((a, b) => a.map((n, i) => n + b[i]), [0, 0])
+				),
+				orderInt: ko.pureComputed(() =>
+					-Math.round(1e5 * self.order().reduce((a, b) => a * 10000 + b, 0))
+				),
+			});
+
+			return self;
+		}
+	}
+
+	function CopyButton(text){
+		let self = {};
+
+		self.text = text;
+		self.done = ko.observable(false);
+
+		self.onClick = () => {
+			copyTextToClipboard(text())
+			self.done(true);
+			setTimeout(() => self.done(false), 1000);
+		}
+
+
+		return {
+			name: "SST-copyButton-template",
+			data: self,
+		}
+	}
+
+	function fallbackCopyTextToClipboard(text) {
+		const el = document.createElement('textarea');
+		el.value = text;
+		el.setAttribute('readonly', '');
+		el.style.position = 'absolute';
+		el.style.left = '-9999px';
+		document.body.appendChild(el);
+		const selected =
+			document.getSelection().rangeCount > 0
+				? document.getSelection().getRangeAt(0)
+				: false;
+		el.select();
+		document.execCommand('copy');
+		document.body.removeChild(el);
+		if (selected) {
+			document.getSelection().removeAllRanges();
+			document.getSelection().addRange(selected);
+		}
+	}
+
+	function copyTextToClipboard(text) {
+		if (!navigator.clipboard)
+			return fallbackCopyTextToClipboard(text);
+
+		navigator.clipboard.writeText(text).then(function() {
+			console.log('Async: Copying to clipboard was successful!');
+		}, function(err) {
+			console.error('Async: Could not copy text: ', err);
+		});
+	}
 });
